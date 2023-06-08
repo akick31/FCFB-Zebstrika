5bdb8e9758092985072820464ff80a7227ebd863
Andrew Kicklighter <apkicklighter@gmail.com>
2023-06-08 01:06:35 -0500

    decrypt and encrypt defensive number

--- src/main/java/com/fcfb/fcfb_zebstrika/controllers/OngoingGamesController.java
+++ src/main/java/com/fcfb/fcfb_zebstrika/controllers/OngoingGamesController.java
@@ -27,8 +27,20 @@ public class OngoingGamesController {
     @Autowired
     TeamsRepository teamsRepository;
 
+    /**
+     * Start a game
+     * @param season
+     * @param subdivision
+     * @param homeTeam
+     * @param awayTeam
+     * @param tvChannel
+     * @param startTime
+     * @param location
+     * @return
+     */
+
     @PostMapping("/games/start/{season}/{subdivision}/{homeTeam}/{awayTeam}/{tvChannel}/{startTime}/{location}")
-    public ResponseEntity<String> startGame(@PathVariable("season") String season,
+    public ResponseEntity<OngoingGamesEntity> startGame(@PathVariable("season") String season,
                                             @PathVariable("subdivision") String subdivision,
                                             @PathVariable("homeTeam") String homeTeam,
                                             @PathVariable("awayTeam") String awayTeam,
@@ -44,7 +56,7 @@ public class OngoingGamesController {
 
                 //TODO update plot names
 
-                ongoingGamesRepository.save(new OngoingGamesEntity(
+                OngoingGamesEntity newGame = ongoingGamesRepository.save(new OngoingGamesEntity(
                         homeTeam,
                         awayTeam,
                         homeTeamData.get().getCoach(),
@@ -79,15 +91,13 @@ public class OngoingGamesController {
                         "none_scoreplot.png",
                         0));
 
-                return new ResponseEntity<>("Game started between " + homeTeam + " and " + awayTeam,
-                        HttpStatus.CREATED);
+                return new ResponseEntity<>(newGame, HttpStatus.CREATED);
             }
             else {
-                return new ResponseEntity<>("Could not find one or both teams (" + homeTeam + " or " + awayTeam
-                        + ") to create the game", HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
             }
         } catch (Exception e) {
-            return new ResponseEntity<>(e.toString(), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
     }
 }
